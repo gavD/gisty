dbb21773f0dff341f20f8fed6f5f4111de40b449
Gavin Davies <gavin@owsy.com>
2022-07-05 19:03:53 +0100

    Adds ESLint

--- README.md
+++ README.md
@@ -3,13 +3,15 @@ Gisty
 
 Gisty is a tool for retrieving a user's gists from Github.
 
+Developed in 2 hours.
+
 System requirements
---
+==
 
 A relatively recent version of NodeJS.
 
 Installation
---
+==
 
 Install dependencies with npm:
 
@@ -18,10 +20,69 @@ npm install
 ```
 
 Usage
---
+==
 
 Get help with:
 
 ```bash
 node index.js -h
 ```
+
+The in-tool help is the best place to start:
+
+```
+Usage: gisty [options] <username>
+
+CLI interface to Github Gists
+
+Arguments:
+  username                     user to get gists for
+
+Options:
+  -V, --version                output the version number
+  -g, --github-token <string>  github_token (env: GITHUB_TOKEN)
+  -a --all                     display all gists (defaults to just the gists created since you last ran the command)
+  -o --output <json|list>      how to display the output (default: "list")
+  -d --debug                   Debug mode - the program will tell you what it is doing
+  -h, --help                   display help for command
+```
+
+About the cache
+--
+
+When you run Gisty, it will show you any gists created since you last ran the tool. You can clear the cache by deleting `.cache/<username>`, which will again return you all the results for the user in question.
+
+Output formats
+--
+
+Two are available - json and list. List is simple and human readable. Json is machine readable and contains a lot more data.
+
+Development
+==
+
+Linting
+--
+
+```bash
+npx eslint index.js
+```
+
+Tool Rationale
+==
+
+Bash would be reasonably straightforward, but string handling in Bash is a nightmare so I decided to use a programming language.
+
+I selected NodeJS simply because it was installed on my laptop. The same could be accomplished with any language. Node's downside is it's "everything and the kitchen sink" packaging approach. Something like Rust could build a tighter, single-binary executable.
+
+If I wanted to go super-overkill, I could dockerise the whole thing. That said, I'd generally prefer an executable over Docker for such a small, sharp tool. I'd have to know the use case to select the most appropriate tooling.
+
+I made the design decision to paginate through all available gists. Otherwise, you'd only get the 30 most recent by default.
+
+I allowed the tool to output either JSON (raw results from Github) or list for a simpler response.
+
+I added a `-d` switch for debug so you can see what the tool is doing in terms of pagination et cetera.
+
+If I had more time...
+==
+
+I would add unit tests. The functions in the global namespace of index.js should be in a module
--- index.js
+++ index.js
@@ -3,101 +3,109 @@ const program = new Command();
 const axios = require('axios');
 const fs = require('fs');
 
-
-function getSince(username) {
-    const path = '.cache/' + username;
-    if (fs.existsSync(path)) {
-        const since = fs.readFileSync(path, {encoding:'utf8', flag:'r'});
-        // console.log({
-        //     since,
-        //     foo: Date.parse(since),
-        //     out: new Date(Date.parse(since) + 1).toISOString()
-        // });
-        //slightly bump it
-        return new Date(Date.parse(since) + 1).toISOString();
-
-    }
-    return null;
+/**
+ * @param {string} username Username of the Github user to check the cache for
+ * @returns {string|null} ISO 8601 date string to filter Gists "since", relevant to the most recently
+ * retrieved data date for user identified by "username"
+ */
+function getSince (username) {
+  const path = '.cache/' + username;
+  if (fs.existsSync(path)) {
+    const since = fs.readFileSync(path, { encoding: 'utf8', flag: 'r' });
+    // slightly bump the date by 1ms - otherwise "since" will match inclusive of the most recent
+    // cached match
+    return new Date(Date.parse(since) + 1).toISOString();
+  }
+  return null;
 }
 
-function cacheResultsToDisk(allResults, username) {
-    // what is the highest number
-    if (allResults.length > 0) {
-        // assumption - the first result returned is the most recent one.
-        // This is what I have observed in all tests.
-        const mostRecentUpdate = allResults[0].created_at;
-        fs.writeFileSync('.cache/' + username, mostRecentUpdate, 'utf8');
-    }
+/**
+ * Write a date to disk so we know the most recently retrieved data date for user identified by "username"
+ *
+ * @param {array} allResults flat array of JSON returned from the Github API
+ * @param {string} username Username of the Github user to cache to disk
+ */
+function cacheResultsToDisk (allResults, username) {
+  // what is the highest number
+  if (allResults.length > 0) {
+    // assumption - the first result returned is the most recent one.
+    // This is what I have observed in all tests.
+    const mostRecentUpdate = allResults[0].created_at;
+    fs.writeFileSync('.cache/' + username, mostRecentUpdate, 'utf8');
+  }
 }
 
-function displayResults(allResults, output) {
-    if ('list' === output) {
-        const data = allResults.map(d => (d.description || '<No description>') + ' - ' +  d.updated_at + "\n  " + d.html_url);
-        console.log(data.join("\n"));
-    } else {
-        console.log(allResults);
-    }
+/**
+ * Output the results to the console
+ *
+ * @param {array} allResults
+ * @param {string} output Either 'list' or 'json
+ */
+function displayResults (allResults, output) {
+  if (output === 'list') {
+    const data = allResults.map(d => (d.description || '<No description>') + ' - ' + d.updated_at + '\n  ' + d.html_url);
+    console.log(data.join('\n'));
+  } else {
+    console.log(allResults);
+  }
 }
 
 program
-    .name('gisty')
-    .description('CLI interface to Github Gists')
-    .version('0.0.1')
-    .addOption(new Option('-g, --github-token <string>', 'github_token').env('GITHUB_TOKEN'))
-    .argument('<username>', 'user to get gists for')
-    .option('-a --all', 'display all gists (defaults to just the gists created since you last ran the command)')
-    .option('-o --output <json|list>', 'how to display the output', 'list')
-    .option('-d --debug', 'Debug mode - the program will tell you what it is doing')
-    .action(async (username, options) => {
-        const results = [];
+  .name('gisty')
+  .description('CLI interface to Github Gists')
+  .version('1.0.0')
+  .addOption(new Option('-g, --github-token <string>', 'github_token').env('GITHUB_TOKEN'))
+  .argument('<username>', 'user to get gists for')
+  .option('-a --all', 'display all gists (defaults to just the gists created since you last ran the command)')
+  .option('-o --output <json|list>', 'how to display the output', 'list')
+  .option('-d --debug', 'Debug mode - the program will tell you what it is doing')
+  .action(async (username, options) => {
+    const results = [];
 
-        const since = getSince(username);
+    const since = getSince(username);
+    if (options.debug) {
+      console.debug({ since });
+    }
 
+    async function getNextPageOfResults (page) {
+      if (options.debug) {
+        console.log('Get page ' + page + '...');
+      }
+      const response = await axios
+        .get(
+          'https://api.github.com/users/' + username + '/gists',
+          {
+            headers: {
+              Accept: 'application/vnd.github+json',
+              Authorization: 'token ' + process.env.GITHUB_TOKEN
+            },
+            params: {
+              page,
+              since
+            }
+          });
+      // add the data to our results
+      results.push(response.data);
 
-        // process.exit(1);
+      // are there any more pages?
+      if (response.data.length === parseInt(options.perPage, 10)) {
         if (options.debug) {
-            console.debug({since});
+          console.log('  ... There are more page options, get another page...')
+        }
+        await getNextPageOfResults(page + 1);
+      } else {
+        if (options.debug) {
+          console.log('  ... There are no more page options');
+          console.log('  ... found ' + results.length + ' pages of results');
         }
 
-        async function getNextPageOfResults(page) {
-            if (options.debug) {
-                console.log("Get page " + page + "...");
-            }
-            const response = await axios
-                .get(
-                    'https://api.github.com/users/' + username + '/gists',
-                    {
-                        headers: {
-                            'Accept': 'application/vnd.github+json',
-                            'Authorization': 'token ' + process.env.GITHUB_TOKEN,
-                        },
-                        params: {
-                            page: page,
-                            since: since
-                        }
-                    });
-            // add the data to our results
-            results.push(response.data);
+        const allResults = [].concat.apply([], results);
+        displayResults(allResults, options.output);
+        cacheResultsToDisk(allResults, username);
+      }
+    }
 
-            // are there any more pages?
-            if (response.data.length === parseInt(options.perPage, 10)) {
-                if (options.debug) {
-                    console.log("  ... There are more page options, get another page...")
-                }
-                await getNextPageOfResults(page + 1);
-            } else {
-                if (options.debug) {
-                    console.log("  ... There are no more page options");
-                    console.log("  ... found " + results.length + " pages of results");
-                }
-
-                const allResults = [].concat.apply([], results);
-                displayResults(allResults, options.output);
-                cacheResultsToDisk(allResults, username);
-            }
-        }
-
-        await getNextPageOfResults(1);
-    });
+    await getNextPageOfResults(1);
+  });
 
 program.parse();
